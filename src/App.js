e5424034d0b9dc48a674e702a0cdb28a7e1289f3
heoseongh <shheo0807@gmail.com>
2024-01-17 18:58:43 +0900

    프록시 설정 추가

--- src/App.js
+++ src/App.js
@@ -1,43 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 
-import React from "react";
-import axios from "axios";
+function App() {
+   const [hello, setHello] = useState('')
 
-function MainPageComponent() {
-  const [products, setProducts] = React.useState([]);
-  React.useState(function () {
-    axios
-      .get(
-        "https://localhost:8080/test"
-      )
-      .then(function (result) {
-        const products = result.data.products;
-        setProducts(products);
-      })
-      .catch(function (error) {
-        console.error(error);
-      });
-  }, []);
+    useEffect(() => {
+        axios.get('/hello')
+        .then(response => setHello(response.data))
+        .catch(error => console.log(error))
+    }, []);
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://naver.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            fetch data: {hello}
+        </div>
+    );
 }
 
-export default MainPageComponent;
+export default App;
